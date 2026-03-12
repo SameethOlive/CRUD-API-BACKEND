d5e86db0f7032d1247458650e35a0b79f0565555
SameethOlive <sameethsoorju2@gmail.com>
2026-03-12 22:54:21 +0200

    Install dotenv
    dotenv Configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Product = require('./models/product.model.js')
 const productRoute=require('./routes/product.route.js')
+require('dotenv').config()
 
 const app = express()
 var myUrl=process.env.URL
