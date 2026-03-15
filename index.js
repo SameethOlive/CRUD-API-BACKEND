90d51d9d970119aff445fb72fc33439266b425e5
SameethOlive <sameethsoorju2@gmail.com>
2026-03-15 18:11:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Product = require('./models/product.model.js')
 const productRoute=require('./routes/product.route.js')
-require('dotenv').config()
+require(`dotenv`).config()
 
 const app = express()
 var myUrl=process.env.URL
+
 
 
 //middleware
@@ -15,15 +16,13 @@ app.use(express.urlencoded({extended: false}))
 //routes
 app.use('/api/products',productRoute)
 
-app.get('/', (req, res) => {
-    res.send("Sonaksha,Oliver")
-})
+
 
 mongoose.connect(myUrl)
     .then(() => {
         console.log("Connected to Database")
-        app.listen(3000, () => {
-            console.log(`Server is running on port ${3000}`)
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
         });
     })
     .catch(() => {
